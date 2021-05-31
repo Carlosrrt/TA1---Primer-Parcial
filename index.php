3ed385edb8e69dce0f3558313e9d9a4de134d746
Carlos Rodriguez <carlosrrt1337@gmail.com>
2021-05-31 20:02:29 -0300

    Primer parcial vercion 1

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ParcialCarlos</title>
+</head>
+<body>
+    <form id="Informacion" action="./index.php" method="post">
+        Nombre: <input type="text" name="nombre" id="nombre"> <br>
+    </form>
+</html>
